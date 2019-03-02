2ff84325f00ec36f66dad107ec3531aedd6c821c
ashleyk <ashleyke200@gmail.com>
2019-03-01 20:13:45 -0800

    add parse2016ElectionResults framework

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -20,4 +21,22 @@ public class Utils {
         return output.toString();
     }
 
+
+    public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
+        ArrayList<ElectionResult> results = new ArrayList<>();
+        String cleanData = cleanData(data);
+        String lines[] = cleanData.split(",");
+
+        for(String s : lines){
+            ElectionResult current = getElectionResult(s);
+            results.add(current);
+        }
+        return results;
+    }
+
+    private static String cleanData(String data) {
+    }
+
+    private static ElectionResult getElectionResult(String s) {
+    }
 }
