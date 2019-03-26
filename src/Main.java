38f0c12854a674d9ef44348b3263334766fdb480
Ashley Ke <ashleyke200@gmail.com>
2019-03-26 10:24:32 -0700

    run the employment/education parsing methods

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        //Test of utils
+        //Test of utils ashleyk
         DataManager data = new DataManager(new ArrayList<State>());
 
         String educationData = Utils.readFileAsString("data/Education.csv");
-        String presidentialData = Utils.readFileAsString("data/2016_Presidential_Results.csv");
         String unemploymentData = Utils.readFileAsString("data/Unemployment.csv");
 
         Utils.parse2016EducationStats(educationData, data);
-        Utils.parse2016PresidentialResults(presidentialData, data);
         Utils.parse2016EmploymentStats(unemploymentData, data);
 
         for(State s : data.getStates()){
@@ -27,18 +25,10 @@ public class Main {
                 Education2016 education = c.getEduc2016();
                 System.out.println("\t\t" + education.toString());
 
-                //test election
-                Election2016 election = c.getVote2016();
-                System.out.println("\t\t" + election.toString());
-
                 //test employment
                 Employment2016 employment = c.getEmploy2016();
                 System.out.println("\t\t" + employment.toString());
             }
         }
     }
-
-
-
-
 }
